09bfc01798584648d110284b92f63342e9822166
Showmick Das <showmickdas75@gmail.com>
2026-05-24 03:20:35 -0700

    Add: majority-element-ii - submission-8

---
+++ Data Structures & Algorithms/majority-element-ii/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        req = len(nums) // 3
+        output = set()
+        mp = {}
+        for num in nums:
+            if num not in mp:
+                mp[num] = 1
+                if mp[num] > req:
+                    output.add(num)
+            else:
+                mp[num] += 1
+                if mp[num] > req:
+                    output.add(num)
+        return list(output)
+
+"""
+- In Majorityn Element I, we kept a leader and it's count. Since the majority
+comes up so many times, that by the end of it, it would still remain a leader,
+even though you would deduct from its count, everytime you saw a non-leader.
+- Apply the same concept of leader and count, but leader added to list, only
+if count exceed a certain threshold.
+- This Boyer-Moore Voting techniques works only for single element. For
+multiple elements it falls short.
+"""
