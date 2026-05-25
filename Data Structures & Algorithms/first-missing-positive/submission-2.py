2c27d89063b8e7572c6b6a7590d0a2c8f33a6d05
Showmick Das <showmickdas75@gmail.com>
2026-05-24 19:42:03 -0700

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        smallest = 10000000
+        for i in range(len(nums)):
+            idx, val = min(enumerate(nums), key=lambda x: x[1])
+            if val >= 0 and val + 1 not in nums:
+                smallest = val
+            else:
+                nums[idx] = 10000000000
+        return smallest + 1
+
+"""
+- You are given an unsorted integer array nums. Return the smallest POS int,
+that is NOT present in nums.
+- O(n) time and O(1) AUXILIARY SPACE, meaning it's not counting the return
+data structure. But then we have to return an int anyways, so it don't matter.
+- nums[i] can be ANY negative or positive integer.
+- Find the smallest positive integer present, and then do 1+ that, make sure
+there is a slot for the 1+ tho, and that the 1+ DOES not exist in the nums.
+"""
+
+"""
+- Keep iterating and find the smallest possible and then return an element
+greater than that by 1.
+"""
