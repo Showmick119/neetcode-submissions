a6c827be32d0a60d12bbb6f044e84d3a86cb4d03
Showmick Das <showmickdas75@gmail.com>
2026-05-25 01:09:57 -0700

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l <= r:
+            if s[r] != s[l]:
+                # trying both options (removing from left side or right side)
+                # if even one successful path is possible from here, we continue
+                # exploring the palindrome
+                # we are no longer restricting and searching just one side, but rather
+                # considering both. if either path tells us we should continue, and that
+                # palindrome exists from here, then we can continue
+                return self.isPalindrome(s[l:r]) or self.isPalindrome(s[l+1:r+1])
+            l += 1
+            r -= 1
+        return True
+    
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l <= r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+        
+"""
+- Can s be a palindrome, after deleting at most 1 character from it?
+- So you would remove while you are in the loop, but how do you know which
+side to remove from?
+- Use i + 1 and check ahead, that if removing it helps continue the palindrome
+or not.
+"""
