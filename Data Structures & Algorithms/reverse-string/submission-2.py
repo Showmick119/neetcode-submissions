0f7ca078e02f5154409a82396ece01908463d326
Showmick Das <showmickdas75@gmail.com>
2026-05-24 20:53:29 -0700

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l = 0
+        r = len(s) - 1
+        while l <= r: ## can swap l and r, even when they are same index
+            # not a problem per say
+            temp = arr[r]
+            arr[r] = arr[l]
+            arr[l] = temp
+            l += 1
+            r -= 1
+
+"""
+- Very obviously a 2-pointers problem.
+"""
