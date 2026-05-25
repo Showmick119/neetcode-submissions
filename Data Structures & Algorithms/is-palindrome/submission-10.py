89e1aea81470dc6ad085324549b3435014b07c80
Showmick Das <showmickdas75@gmail.com>
2026-05-24 21:11:24 -0700

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l <= r:
+            if not s[l].isalnum():
+                l += 1
+            if not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+"""
+- O(n) time comp and O(1) space comp.
+- Case insensitive so use some char lower method lil bro.
+- Only considers alphanumeric characters.
+"""
