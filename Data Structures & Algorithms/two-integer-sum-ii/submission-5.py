006c1fe176aa18483fa0b38dd49dff2ab3f284af
Showmick Das <showmickdas75@gmail.com>
2026-05-26 00:55:46 -0700

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        idx1 = 0
+        idx2 = 0
+        while idx2 < len(numbers):
+            if idx2 <= idx1:
+                idx2 += 1
+                continue
+            if numbers[idx1] + numbers[idx2] == target:
+                return [idx1 + 1, idx2 + 1]
+            if idx2 == len(numbers) - 1:
+                idx1 += 1
+                idx2 = 0
+            else:
+                idx2 += 1
+        return []
+
+
+        # for idx1 in range(len(numbers)):
+        #     for idx2 in range(len(numbers)):
+        #         if idx2 <= idx1:
+        #             continue
+        #         if numbers[idx1] + numbers[idx2] == target:
+        #             return [idx1 + 1, idx2 + 1]
+        # return []
+
+"""
+- The final output data structure doesn't count in 
+"""
