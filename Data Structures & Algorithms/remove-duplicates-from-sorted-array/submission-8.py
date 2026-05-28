027fcd6aba8b6101050e378abab1c7dec8af033e
Showmick Das <showmickdas75@gmail.com>
2026-05-28 00:22:29 -0700

    Add: remove-duplicates-from-sorted-array - submission-8

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 0 # first element always unique
+        write = 1
+        count = 0
+        for i in range(1, len(nums), 1):
+            if nums[i] == nums[k]:
+                count += 1
+                k = i
+            else:
+                nums[write] = nums[i]
+                write += 1
+                k = i
+        return len(nums) - count
+
+"""
+- Think about it this way. You don't actually remove any elements. You just need the
+first k elements to be the unique ones.
+- The first element is always unique.
+- When you find a new unique element, where should you place it?
+"""
