d6e6ef04d753bf0c71093a0c1aa6d7474e1b3d62
Showmick Das <showmickdas75@gmail.com>
2026-05-30 10:10:47 -0700

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.py
@@ -0,0 +1,26 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        area = 0
+        # keep a left pointer to keep track of largest element from left so far
+        l = 0
+        # keep a right pointer to keep track of largest element from right so far
+        r = len(height) - 1
+        leftMax = rightMax = 0
+        while l < r:
+            leftMax = max(height[l], leftMax)
+            rightMax = max(height[r], rightMax)
+            if leftMax < rightMax:
+                area += max((min(leftMax, rightMax) - height[l]), 0)
+                l += 1
+            else:
+                area += max((min(leftMax, rightMax) - height[r]), 0)
+                r -=1
+        return area
+
+"""
+- In Brute force, you can tell and realize that there is a lot of repeated work.
+- Identify this repeated work and compress/remove it systematically.
+- So you decide to keep a prefix array, which tells you the maximum value up and till
+that specific index.
+- First brute force, then identify repeated work and then compress/remove that work.
+"""
