f31f23fb2c57c1629e375a6a1e24a9c3f0485650
Showmick Das <showmickdas75@gmail.com>
2026-05-30 09:02:05 -0700

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        area = 0
+        for i in range(len(height)):
+            if i == 0 or i == len(height) - 1:
+                continue
+            # find max wall on left
+            leftMax = max(height[0:i])
+            # find max wall on right
+            rightMax = max(height[i:])
+
+            area += max((min(leftMax, rightMax) - height[i]), 0)
+        return area
+
+"""
+- You are calculating the water at each index individually.
+- You are not trying to find multiple good windows per say. You are just trying to
+find the water value at each particular inxex. Given that the bar value at index i
+is less than both the maxLeft and maxRight.
+- We are not finding basins or windows, stop thinking like that. We are individually
+adding water at each index.
+- For each index i, find the max wall on left and right.
+"""
