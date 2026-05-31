8d7f875ab93404f001f200592b0b46f4c820c6a3
Showmick Das <showmickdas75@gmail.com>
2026-05-31 11:17:25 -0700

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        count = 0
+        seq = set(nums)
+        for num in nums:
+            if ((num - 1) in seq):
+                continue
+            else:
+                seq.add(num)
+                count += 1
+        return count
+
+"""
+- They don't care about relative ordering.
+"""
