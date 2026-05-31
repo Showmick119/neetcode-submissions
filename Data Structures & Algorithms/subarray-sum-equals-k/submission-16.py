8be0e1f583e316b6cf989bab237e5359a7a91907
Showmick Das <showmickdas75@gmail.com>
2026-05-31 13:41:09 -0700

    Add: subarray-sum-equals-k - submission-16

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-16.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        mp = {}
+        run = 0
+        count = 0
+        mp[0] = 1
+        for num in nums:
+            if run - k in mp:
+                count += mp[run - k]
+            if run not in mp:
+                mp[run] = 1 # one sub array which has this sum. and prefix sums are sum of elements before that particular index i
+            else:
+                mp[run] += 1
+            run += num
+        return count
+
+"""
+- prefixSum[i] - prefixSum[j] = k
+- prefixSum[i] - k = prefixSum[j], then the subarray betwen indices i and j are also equal
+to k.
+- Store the subarray sums as keys in the map, and then the value should be there quantity.
+"""
