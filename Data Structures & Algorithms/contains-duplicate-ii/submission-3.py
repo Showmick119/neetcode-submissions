f2afd1a2a3e8f179d074a4c5b051d54c73c92379
Showmick Das <showmickdas75@gmail.com>
2026-06-05 10:00:03 -0700

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        from collections import deque
+        w = deque() # our window which should not contain more than k elements
+        for num in nums:
+            if len(w) >= k + 1:
+                w.popleft() # if its at max window length of k + 1, pop an element such that
+                # we have room for the next element which we are adding in this loop. upon
+                # adding that next element, we still maintain that max window length of k + 1.
+            if num in w:
+                return True
+            w.append(num)
+        return False
+
+"""
+- abs(i - j) <= k, means its in a window of (k + 1) elements, and the elements can be at most
+k distance apart.
+- [0, 1, 2, 3] is a valid window of k + 1 elements, as the farthest pair, at idx = 0 and
+idx = 3 are k distance apart.
+"""
