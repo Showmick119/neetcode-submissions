f05a03685beabf24e04303a7b956157e11c3f15f
Showmick Das <showmickdas75@gmail.com>
2026-06-05 14:41:11 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        best = 0
+        l = 0
+        idx, s = min(enumerate(prices), key=lambda x: x[1])
+        for r in range(idx + 1, len(prices)):
+            curr = prices[r] - prices[idx]
+            if curr > best:
+                best = curr
+        return best
+
+"""
+- we have to create a window nonetheless, but start off with the smallest possible l value,
+and then find the largest possible r value.
+
+HOW TO USE LAMBDA FUNCTION:
+lambda parameter: value_to_return
+
+lambda x: x[1]
+lambda x: abs(x)
+lambda x: x.name
+
+
+With min, max, sort, sorted:
+max(items, key=lambda x: x[1])
+
+For each item x, use x[1] as the comparison value. The lambda doesn't do the
+sorting/min/max itself. It just tells Python what value to compare by.
+"""
