66fad6522825ec1fabacaf582f3fb73aba034504
Showmick Das <showmickdas75@gmail.com>
2026-06-14 13:03:23 -0700

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        from collections import deque
+        w = deque()
+        for r in range(len(s2)):
+            if len(w) == len(s1):
+                if "".join(sorted(w)) == "".join(sorted(s1)):
+                    return True
+                else:
+                    w.popleft()
+            w.append(s2[r])
+        return False
+
+"""
+- you are given 2 strings s1 and s2, return true if s2 (the larger string) contains a
+permutation of s1 (the smaller string) as its substring.
+- a permutation is a rearrangement of the same elements. but when those same elements are
+sorted, that arrangement becomes the same and the permutations become equal.
+- go through s2, and each time you have a window that is equal in length to the s1 string.
+sit down and compare them. if same permutation return true, if not, cut from the back, and
+add to the front and keep going over the s2.
+- we need to aim for o(n) time comp and o(1) space comp. how do we check for permutation
+between 2 strings? do a 2 pointers a frequency count? but that would need another data
+structure? start of with the naive solution
+"""
