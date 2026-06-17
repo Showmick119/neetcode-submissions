0836c80cb65c2b4a837781236c4a1d28f6477a72
Showmick Das <showmickdas75@gmail.com>
2026-06-16 21:18:12 -0700

    Add: minimum-size-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        from collections import deque
+        w = deque()
+        l = 0
+        smallest = 1000000000
+        for r in range(len(nums)):
+            if sum(w) >= target:
+                while sum(w) >= target:
+                    smallest = min(smallest, len(w))
+                    w.popleft()
+            else:
+                w.append(nums[r])
+        if smallest == 1000000000:
+            return 0
+        else:
+            return smallest
+
+"""
+- you are given an array of positive integers nums, and a positive integer target, return
+the minimal length of a subarray whose sum is greater than or equal to target.
+- if there is no such subarray, return 0.
+- a subarray is a contiguous non-empty sequence of elements within an array
+"""
