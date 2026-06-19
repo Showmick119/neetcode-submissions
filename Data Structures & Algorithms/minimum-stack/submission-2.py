d426d5b59f569c985a974f230eeec75e35756542
Showmick Das <showmickdas75@gmail.com>
2026-06-18 21:58:43 -0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        from collections import deque
+        self.small = deque()
+        self.smallest = 100000000
+
+    def push(self, val: int) -> None:
+        if val < self.smallest:
+            self.smallest = val
+            self.small.appendleft(val)
+        else:
+            self.small.append(val)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.top() == self.small[0]:
+            self.small.popleft()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.small[0]
+
+"""
+- it has to be designed in a way, such that the minimum element is always at the top of the
+stack, so getMin() is really just a call to top().
+"""
