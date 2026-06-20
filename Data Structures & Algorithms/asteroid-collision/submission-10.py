2c9ac318a2f2569372fd10bcf43e111814c1bd6a
Showmick Das <showmickdas75@gmail.com>
2026-06-19 21:29:53 -0700

    Add: asteroid-collision - submission-10

---
+++ Data Structures & Algorithms/asteroid-collision/submission-10.py
@@ -0,0 +1,24 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        stack.append(asteroids[0])
+        i = 1
+        while len(stack) > 0 and i < len(asteroids) and ((stack[-1] > 0 and asteroids[i] < 0) or (stack[-1] < 0 and asteroids[i] > 0)):
+            # WHILE A COLLISION IS STILL POSSIBLE
+            # IF NOT POSSIBLE, YOU HAVE YOUR ANSWER
+            if abs(stack[-1]) == abs(asteroids[i]):
+                stack.pop()
+            elif abs(stack[-1]) < abs(asteroids[i]):
+                stack.pop()
+                stack.append(asteroids[i])
+            else:
+                pass
+            i += 1
+        return stack
+
+"""
+- approach it kind of like valid parentheses, where you push and pop and cancel out as you go.
+- you are just doing the 1 v 1 and then forgetting about it. you have to keep propagating and
+doing the 1 v 1.
+- don't think if collision possible. think WHILE collision possible;
+"""
