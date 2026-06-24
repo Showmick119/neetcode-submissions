c835a59aeadd9b41c84e186e8f610ebaafcbea64
Showmick Das <showmickdas75@gmail.com>
2026-06-23 23:23:10 -0700

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        for i in range(len(position)):
+            curr_time = (target - position[i]) / speed[i]
+            position[i] = (position[i], curr_time)
+        position.sort(key=lambda x: x[0], reverse=True)
+        stack = []
+        fleet = 0
+        for i in range(len(position)):
+            if len(stack) > 0:
+                if position[i][1] <= stack[-1]:
+                    pass
+                else:
+                    stack.pop() # new leader
+                    stack.append(position[i][1])
+                    fleet += 1 # arriving at a different time
+            else:
+                stack.append(position[i][1])
+                fleet += 1
+        return fleet
+
+"""
+- there are n cars travelling to the same destination on a one-lane highway.
+- you are given two arrays of integers position and speed.
+- the destination is at position target miles.
+- a car cannot pass another car ahead of it, it can only catch up to another car and then
+drive at the same speed as the car ahead of it.
+"""
