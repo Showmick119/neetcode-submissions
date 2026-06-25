91e9efc21f443856896caeb1a7ea95ffb81d7ac0
Showmick Das <showmickdas75@gmail.com>
2026-06-24 23:05:48 -0700

    Add: decode-string - submission-4

---
+++ Data Structures & Algorithms/decode-string/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if c == ']':
+                curr_string = ""
+                while stack[-1] != '[':
+                    curr_string = stack.pop() + curr_string
+                stack.pop()
+                curr_num = ""
+                while len(stack) > 0 and stack[-1].isdigit(): ## keep doing so while its still a number to *
+                    curr_num = stack.pop() + curr_num
+                curr_string *= int(curr_num)
+                for i in curr_string:
+                    stack.append(i)
+                # stack.append(curr_string)
+            else:
+                stack.append(c)
+        out = "".join(stack)
+        return out
