10f3ea9a6b3ab15e659671a524365c6208bc7f9e
Showmick Das <showmickdas75@gmail.com>
2026-06-28 19:56:53 -0700

    Add: maximum-frequency-stack - submission-3

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-3.py
@@ -0,0 +1,28 @@
+class FreqStack:
+
+    def __init__(self):
+        self.stack = []
+        self.freq = {}
+
+    def push(self, val: int) -> None:
+        if val in self.freq:
+            self.freq[val] += 1
+        else:
+            self.freq[val] = 1
+        self.stack.append((val, self.freq[val]))
+
+    def pop(self) -> int:
+        self.stack.sort(key=lambda x: x[1])
+        curr = self.stack.pop()
+        self.freq[curr[0]] -= 1
+        return curr[0]
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
+
+"""
+- naive solution is easy, solve it naively first, and then worry about optimality
+"""
