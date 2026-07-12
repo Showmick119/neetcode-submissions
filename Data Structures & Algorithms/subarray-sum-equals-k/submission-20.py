e2c5c24bf7140ef102835b12ea7c5bdc9d710656
Showmick Das <showmickdas75@gmail.com>
2026-07-12 13:38:36 -0700

    Add: subarray-sum-equals-k - submission-20

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-20.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        runningSum = 0
+        sumMap = {}
+        sumMap[0] = 1
+        for num in nums:
+            runningSum += num
+            if runningSum not in sumMap:
+                sumMap[runningSum] = 1
+            else:
+                sumMap[runningSum] += 1
+            if runningSum - k in sumMap:
+                count += sumMap[runningSum - k]
+        return count
