a0dd4ba9f20299bb234a5676a7a1d4e33aa2fba9
Showmick Das <showmickdas75@gmail.com>
2026-07-11 22:49:28 -0700

    Add: longest-repeating-substring-with-replacement - submission-21

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-21.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        from collections import deque
+        window = deque()
+        longest = 0
+        mostFreq = 0
+        mp = {}
+        for char in s:
+            window.append(char)
+            if char not in mp:
+                mp[char] = 1
+            else:
+                mp[char] += 1
+            mostFreq = max(mostFreq, mp[char])
+            if len(window) - mostFreq <= k:
+                longest = max(longest, len(window))
+            else:
+                mp[char] -= 1
+                window.popleft()
+        return longest
+
+"""
+- need to keep track of valid substrings which meet a certain condition.
+"""
