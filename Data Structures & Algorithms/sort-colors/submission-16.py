c24407462fe1fcfa2f3e6a42f585356cc9d761f7
Showmick Das <showmickdas75@gmail.com>
2026-07-13 07:51:03 -0700

    Add: sort-colors - submission-16

---
+++ Data Structures & Algorithms/sort-colors/submission-16.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        redPointer = 0
+        bluePointer = len(nums) - 1
+        i = 0
+        while i < len(nums):
+            if nums[i] == 0:
+                temp = nums[redPointer]
+                nums[redPointer] = nums[i]
+                nums[i] = temp
+                redPointer += 1
+                i += 1
+            elif nums[i] == 2:
+                temp = nums[bluePointer]
+                nums[bluePointer] = nums[i]
+                nums[i] = temp
+                bluePointer -= 1
+            else:
+                i += 1
+
+"""
+- it's a two pointers question, we have one at the front and another at the back. one for the
+red group and the other for the blue group
+"""
