5a6178645065f14a99bfde2bab979a5d71272289
Showmick Das <showmickdas75@gmail.com>
2026-07-12 21:14:28 -0700

    Add: trapping-rain-water - submission-21

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-21.py
@@ -0,0 +1,37 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        maxArea = 0
+        l, r = 0, len(height) - 1
+        maxLeft, maxRight = height[l], height[r]
+        while l < r:
+            maxLeft = max(maxLeft, height[l])
+            maxRight = max(maxRight, height[r])
+            curr = height[l]
+            areaHeight = min(maxLeft, maxRight)
+            print(f'maxLeft = {maxLeft}, maxRight = {maxRight}, curr = {curr}, areaHeight = {areaHeight}')
+            maxArea += max(0, areaHeight - curr)
+            if maxLeft > maxRight:
+                r -=1
+            l += 1
+        return maxArea
+
+
+        # for i in range(len(height)):
+        #     if i == len(height) - 1 or i == 0:
+        #         continue
+        #     curr = height[i]
+        #     maxL = max(height[0:i])
+        #     maxR = max(height[i+1:])
+        #     print(f'maxL: {maxL} and maxR: {maxR}')
+        #     h = min(maxL, maxR)
+        #     max_area += max(0, h - curr)
+
+        
+"""
+- we keep a left and right pointer, and we also keep 2 variables which store the max_left and
+max_right pointers. while the left pointer would determine the left boundary.
+- in terms of iterating through the array, you would use the left pointer to go through it,
+while storing the maxLeft pointer for our left most boundary in between which water would be
+stored.
+- the naive is to find the MAX RIGHT/LEFT boundary at each index of the array
+"""
