62c04515e1675e6251fe7491f19c0a46ed5243fb
Showmick Das <showmickdas75@gmail.com>
2026-07-18 11:14:42 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # l = 0
+        # r = len(nums) - 1
+        # while l <= r
+        return min(nums)
+
+"""
+- its still binary search, but now the indexes will wrap around using the remainder function
+"""
