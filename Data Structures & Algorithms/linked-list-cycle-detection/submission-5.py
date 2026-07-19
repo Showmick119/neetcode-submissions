9f8aaa75921c57d6fb8577bffa74f2b654ecd75e
Showmick Das <showmickdas75@gmail.com>
2026-07-19 01:12:42 -0700

    Add: linked-list-cycle-detection - submission-5

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-5.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+        curr = head
+        while curr != None:
+            if curr.next in seen:
+                return True
+                # return visitMap[curr.val] # will give its index
+            else:
+                seen.add(curr) # don't add no index nor value, add the actual object itself,
+                # such that you can properly equate and check for pointers
+            curr = curr.next
+        return False
+
+"""
+- we can trivially keep a hashset, but that goes against the o(1) space complexity
+"""
