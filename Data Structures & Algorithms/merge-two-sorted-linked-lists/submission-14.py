726696ca90bddc19d566ad0a4e1e52b0ac04b58a
Showmick Das <showmickdas75@gmail.com>
2026-07-19 00:20:37 -0700

    Add: merge-two-sorted-linked-lists - submission-14

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-14.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None and list2 is None:
+            return None # edge case
+        dummy = node = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val: # we process and get rid of the smallest values
+                node = list1
+                list1 = list1.next
+            else:
+                node = list2
+                list2 = list2.next
+            node = node.next # won't throw NPE, as we are never None, and always pointing to an
+            # existing list with continuing chained Nodes/values
+        node.next = list1 or list2 # not a single
+        return dummy.next # the head of the node list we just created without introducting a 
+        # brand new list number 3!
+        # dummy was also pointing to the same thing node was. but node went on and built the
+        # whole linked list, while dummy was left behind and kept pointing at the dummy. but
+        # dummy.next pointed to the head of the list which node iteratively created. so dummy
+        # was actually holding the head of the linked list all along.
+
+"""
+- returning the head of a LL, returns the LL itself
+"""
