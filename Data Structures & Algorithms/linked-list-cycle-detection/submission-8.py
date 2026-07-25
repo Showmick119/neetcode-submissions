acbfd3b260e51afb1b721bb4cff4d973af3ad479
Showmick Das <showmickdas75@gmail.com>
2026-07-25 06:27:20 -0700

    Add: linked-list-cycle-detection - submission-8

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-8.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while slow.next != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+"""
+- use fast and slow pointers technique
+- it won't be an infinite loop if its a normal list, it will only go on for a while if it is
+a cylic list
+"""
