669cd01ea598d4faed046648e9afe0dc5f55c823
Showmick Das <showmickdas75@gmail.com>
2026-08-31 17:27:52 -0400

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}
+        for index, num in enumerate(nums):
+            mp[num] = index # keep the latest ones, as the earlier ones will be processed in
+            # the next loop
+        for index, num in enumerate(nums):
+            if target - num in mp:
+                if index < mp[target - num]:
+                    return [index, mp[target - num]]
+                else:
+                    return [mp[target - num], index]
+        return []
